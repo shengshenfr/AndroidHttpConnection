744ae714fff94f1d63b5d25e069e15a8446c5b7d
shengshenfr <sheng.shen@telecom-bretagne.eu>
2017-07-08 22:03:50 +0200

    Add files via upload
    correct the app

--- InternetRequest-master/app/src/main/java/com/example/android/internetrequest/MainActivity.java
+++ InternetRequest-master/app/src/main/java/com/example/android/internetrequest/MainActivity.java
@@ -20,47 +20,59 @@ import java.util.List;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+    //create the button and textview for the UI
     private Button mButton;
     private TextView mResult;
+
+    //create url
     private  String request ="http://api.openweathermap.org/data/2.5/forecast/daily?q=NOM_VILLE&cnt=14&mode=json&units=metric&lang=fr";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //put button amd result in the UI
         mButton = (Button) findViewById(R.id.bt_search);
         mResult = (TextView) findViewById(R.id.tv_result);
+        //realize the listener for button
         mButton.setOnClickListener( new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                //execute the URL
                 new SearchTask().execute(request);
 
             }
         });
     }
+
+
+    //create the async task
     private class SearchTask extends AsyncTask<String, Void, String>{
         private String requestURL;
         @Override
+        //do some markup before the background task
         protected void onPreExecute() {
             super.onPreExecute();
         }
 
         @Override
+        //When the background operation ends, the result is displayed directly on the UI component.
         protected void onPostExecute(String response) {
             super.onPostExecute(response);
-
+            //create the object json
             Gson gson = new Gson();
             Log.w("AAAA",response);
+            //change json data to another type of data
             Jsondataformat result = gson.fromJson(response,Jsondataformat.class);
             Log.w("AAAA","~~~~~~~~~~~~~~~~~~```");
             response = "cod: "+ String.valueOf(result.cod) + "\nmessage: "+result.message;
 
             Log.w("AAAA","~~~~~~~~~~~~~~~~~~```");
-            mResult.setText(response);
+            mResult.setText(response);//put it in the view
         }
 
         @Override
+        //create the httpConnection
         protected String doInBackground(String... params) {
             InputStream in = null;
 
@@ -80,17 +92,20 @@ public class MainActivity extends AppCompatActivity {
             }
 
             try {
+                // if response code is 200, use get input stream
                 in = urlConnection.getInputStream();
 
             } catch (IOException e) {
+                //if response code is 401 unauthorized, catch the exception
                 in = urlConnection.getErrorStream();
 
             } finally {
                 urlConnection.disconnect();
 
             }
+            //read the data
             Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
+            scanner.useDelimiter("\\A");//use A as a separator
             boolean hasInput = scanner.hasNext();
             if (hasInput) {
                 return scanner.next();
